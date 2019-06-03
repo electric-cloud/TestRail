e039a2aada2c45fb4b09dcde168ad8ba00bff5ea
Pavel-vovk <wolf.od@gmail.com>
2019-06-03 17:20:53 +0300

    change pm

--- dsl/procedures/ImportTestAsJSONtoTestRail/steps/ImportTestAsJSONtoTestRail.pl
+++
@@ -1,6 +0,0 @@
-$[/myProject/perl/core/scripts/preamble.pl]
-
-use FlowPlugin::TestE2EPlugin;
-# Auto generated code of plugin step
-# Go to dsl/properties/perl/lib/EC/Plugin/TestE2EPlugin.pm and change the function "importTestAsJSONToTestRail"
-FlowPlugin::TestE2EPlugin->runStep('Import Test As JSON to TestRail', 'Import Test As JSON to TestRail', 'importTestAsJSONToTestRail');
